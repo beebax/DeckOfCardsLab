a8eb0796b73ac8cac863f6e0a2ce997198071f7c
belle <belle.baxley@gmail.com>
2023-02-13 18:36:15 -0500

    first commit

---
+++ DeckOfCardsLab/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DeckOfCardsLab.Models;
+
+
+namespace DeckOfCardsLab.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult DrawHand()
+    {
+        //Created new instance of Deckofcardsdal class, and saved it to deckofcardsDAL
+        DeckOfCardsDAL deckOfCardsDAL = new DeckOfCardsDAL();
+        //Calling GetCards function on deckOfCardsDAL, and assigning it to deckOfCards
+        DeckOfCardsModel deckOfCards = deckOfCardsDAL.GetCards();
+        return View(deckOfCards);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ DeckOfCardsLab/Models/DeckOfCardsDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+using DeckOfCardsLab.Models;
+
+namespace DeckOfCardsLab.Models
+{
+    public class DeckOfCardsDAL
+    {
+        public DeckOfCardsModel GetCards()
+        {
+            //Setup
+            string url = $"https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+
+            //Request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Convert to C#
+            DeckOfCardsModel deckOfCards = JsonConvert.DeserializeObject<DeckOfCardsModel>(JSON);
+            return deckOfCards;
+
+        }
+    }
+}
+
